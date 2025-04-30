e8c58e0ad5c1d3aab943a25ccaa1b0c44dea6010
ysg <didtmdrb0930@naver.com>
2025-04-30 16:51:00 +0900

    init 안해서 전체로 잡혔었기에 다시 수정했습니다.

---
+++ src/main/java/com/example/MyDiary/MyDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.example.MyDiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyDiaryApplication.class, args);
+	}
+
+}
